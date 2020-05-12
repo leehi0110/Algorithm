d0a736c4953a66d04d0eec0393abad6fb668f3d9
leehi0110 <maple0110@naver.com>
2020-05-12 15:08:59 +0900

    2weeks

---
+++ SchoolAlgorithm/C_Programming/2weeks/2-1.cpp
@@ -0,0 +1,22 @@
+// 2주차
+// 변수와 자료형
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // ios_base :: sync_with_stdio(false);
+    // cin.tie(NULL);
+    int a,b;
+
+    cout << "first int: ";
+    cin >> a;
+    cout << "Second int: ";
+    cin >> b;
+
+    cout << "Sum = " << a+b;
+}
