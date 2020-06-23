b05609114bd2ba091784440d180dc4017dffb801
leehi0110 <maple0110@naver.com>
2020-06-23 21:17:59 +0900

    2020/06/23 Programmers Algorithm
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함2020/06/23 Programmers Algorithm
    
    vector<pair<int, int>> 를 사용하여 시간이 1초 지나가는 모든 경우를 따져
    다리를 건너기 위한 총 시간을 구함

--- ProgrammersAlgorithm/Level_2/L2_Truck_Passing_A_Bridge.cpp
+++ ProgrammersAlgorithm/Level_2/L2_Truck_Passing_A_Bridge.cpp
@@ -1,97 +1,34 @@
 #include <string>
 #include <vector>
-#include <deque>
-#include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int solution(int time, int weight, vector<int> truck_weights) {
-    int answer = 0;
-    deque <int> times;
-    deque <int> ontheBridge;
-    
-    times.push_back(time+1); // 시간 추가
-    weight -= truck_weights[0]; // 남은 다리 무게 계산
-    ontheBridge.push_back(truck_weights[0]); // 다리에 있는 트럭의 무게들
-    truck_weights.erase(truck_weights.begin()); // 남은 트럭
-    
-    cout << "truck weights = ";
-    for(int i : truck_weights){
-        cout << i << " ";
-    }
-    cout << endl;
-    
-    cout << "time stack = ";
-    for(int i : times){
-        cout << i << " ";
-    }
-    cout << endl;
-    
-    cout << "ontheBridge = ";
-    for(int i : ontheBridge){
-        cout << i << " ";
-    }
-    cout << endl;
-    
-    while(!truck_weights.empty()){
-        if(weight - truck_weights[0] >= 0){ // 다리에 트럭이 더 올라갈 수 있는 경우
-            weight -= truck_weights[0]; // 남은 다리의 무게
-            ontheBridge.push_back(truck_weights[0]); // 다리에 있는 트럭의 무게들
-            times.push_back(times.back()+1); // 시간 추가
-            
-            truck_weights.erase(truck_weights.begin()); // 남은 트럭
-            
-                cout << "truck weights = ";
-                for(int i : truck_weights){
-                    cout << i << " ";
-                }
-                cout << endl;
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int time = 0;
+    vector <pair<int,int>> truck;
 
-                cout << "time stack = ";
-                for(int i : times){
-                    cout << i << " ";
-                }
-                cout << endl;
+    while(1){
 
-                cout << "ontheBridge = ";
-                for(int i : ontheBridge){
-                    cout << i << " ";
-                }
-                cout << endl;
-        }
-        else { // 다리에 트럭이 더 이상 올라갈 수 없는 경우
-            weight += ontheBridge.front();
-            ontheBridge.pop_front();
-            
-            times.push_back(times.back()+(times.back()-times.front())+time);
-            times.pop_front();
-            
+        if(!truck_weights.empty() && weight >= truck_weights[0]){
             weight -= truck_weights[0];
-            ontheBridge.push_back(truck_weights[0]);
-            truck_weights.erase(truck_weights.begin());
-            
-            cout << "truck weights = ";
-            for(int i : truck_weights){
-                cout << i << " ";
-            }
-            cout << endl;
+            truck.push_back(pair<int,int>(truck_weights[0],0));
 
-            cout << "time stack = ";
-            for(int i : times){
-                cout << i << " ";
-            }
-            cout << endl;
-
-            cout << "ontheBridge = ";
-            for(int i : ontheBridge){
-                cout << i << " ";
-            }
-            cout << endl;
+            truck_weights.erase(truck_weights.begin());            
         }
+
+        for(int i=0;i<truck.size();i++){
+            truck[i].second++;
+        }
+
+        if(truck[0].second == bridge_length){
+            weight += truck[0].first;
+            truck.erase(truck.begin());
+        }
+        time ++;
+
+        if(truck.empty() && truck_weights.empty()) break;
+
     }
-    
-    answer = times.back();
-    
-    return answer;
+
+    return ++time;
 }
