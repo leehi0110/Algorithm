f297900525c56b7c496cb20c1435a0819594a636
leehi0110 <maple0110@naver.com>
2020-06-23 19:55:22 +0900

    2020/06/23 Programmers Algorithm
    stack을 이용하여 짝을 맞춘 뒤 제거
    문자열의 길이만큼 반복문을 다 돈 뒤
    스택이 비어있지 않으면 짝이 맞지 않는 것

--- ProgrammersAlgorithm/Level_2/L2_Remove_In_Pairs.cpp
+++ ProgrammersAlgorithm/Level_2/L2_Remove_In_Pairs.cpp
@@ -1,52 +1,26 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
-
+#include<string>
+#include<stack> // check
 using namespace std;
 
 int solution(string s)
 {
-    int original;
-    int deleteLen = s.length();
-    
-    do {
-        original = deleteLen;
-        
-        cout << "before s = " << s << endl;
-        
-        for(int i=0;i<s.length()-1;i++){
-            if(s[i] == s[i+1]){
-                s.erase(s.begin()+i,s.begin()+i+2);
-                cout << "after s = " << s << endl;        
-                i--;
+    stack<char> Stack;
+
+    for(int i =0;i<s.length();i++){
+        if(Stack.empty())
+            Stack.push(s[i]);
+        else{
+            if(Stack.top() == s[i]){
+                Stack.pop();
             }
-
-            if(s == "") break;
+            else{
+                Stack.push(s[i]);
+            }
         }
-        
-        if(s == "") break;
-        
-        cout << "after after s = " << s << endl;
-        deleteLen = s.length();
-        
-    } while( deleteLen != original || deleteLen == 0);
-    
-    return s == "" ? 1 : 0;
+    }
+    if(Stack.empty())
+        return 1;
+    else 
+        return 0;    
 }
-
-int main()
-{
-    cout << solution("baabaa") << endl;
-    cout << solution("cdcd") << endl;
-}
-
-//         for(int i=0;i<s.length()-1;i++){
-//             if(s[i] == s[i+1]) {
-//                 s.erase(s.begin()+i,s.begin()+i+2);
-//                 i = -1;
-//             }
-            
-//             if(i == s.length()-1) break;
-//         }
-    
-//     return s == "" ? 1 : 0;
