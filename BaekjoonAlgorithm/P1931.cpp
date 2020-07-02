7fca7b2f0c5319ecb0160007f6287216c53d49f0
leehi0110 <maple0110@naver.com>
2020-07-02 17:07:33 +0900

    2020/07/02 Baekjoon Algorithm 회의실 배정 -> 재귀부분 재작성 필요

--- BaekjoonAlgorithm/P1931.cpp
+++ BaekjoonAlgorithm/P1931.cpp
@@ -8,16 +8,12 @@
 #include <map>
 
 using namespace std;
-int check_conference_cnt(int start_time,multimap<int,int> &time_table,multimap<int,int>::iterator &iter);
+int recur_conference(int start_time, multimap<int,int>::iterator &iter, multimap<int,int> &time_table);
 
 int main()
 {
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-
     int conference_num;
     int max_conference = 0;
-
     multimap <int,int> time_table;
     multimap <int,int>::iterator iter;
 
@@ -25,33 +21,34 @@ int main()
 
     for(int i=0;i<conference_num;i++){
         int start_time, end_time;
-
         cin >> start_time >> end_time;
+
         time_table.insert(pair<int,int>(start_time,end_time));
     }
 
     for(iter = time_table.begin();iter != time_table.end();iter++){
-        int count_conference = 1;
+        int check_conference = 1;
+        check_conference += recur_conference(iter->second,iter,time_table);
 
-        count_conference += check_conference_cnt(iter->second,time_table,iter);
-        if(max_conference < count_conference) max_conference = count_conference;
+        if(max_conference < check_conference) max_conference = check_conference;
     }
-    cout << max_conference;
+
+    cout << max_conference << endl;
 }
 
-int check_conference_cnt(int start_time,multimap<int,int> &time_table,multimap<int,int>::iterator &iter)
+int recur_conference(int start_time, multimap<int,int>::iterator &iter, multimap<int,int> &time_table)
 {
-    int recur_max_cnt = 0;
-    int tmp_cnt;
-
+    cout << " now start_time << " << start_time << endl;
+    int check_conference = 0;
     for(iter;iter != time_table.end();iter++){
         if(iter->first >= start_time) {
-            tmp_cnt = 1 + check_conference_cnt(iter->second,time_table,iter);
+            cout << "next start time = " << iter->first << endl;
+            int tmp = 1 + recur_conference(iter->second,iter,time_table);
+            if(check_conference > tmp) {
+                check_conference = tmp;
+            }
         }
-        if(tmp_cnt > recur_max_cnt) {
-            return recur_max_cnt > tmp_cnt ? recur_max_cnt : tmp_cnt;
-        }    
+        return check_conference;
     }
-
-    return recur_max_cnt;
+    return 0;
 }
