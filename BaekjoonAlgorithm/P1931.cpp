5d8e36e0da7495e312df4b14acf29f72c834adb6
leehi0110 <maple0110@naver.com>
2020-07-07 15:19:35 +0900

    2020/07/07 Baekjoon Algorithm 그리디 - 회의실 배정
    문제를 해결하기 위해선 시작시간을 기준으로 정렬하는 것이 아닌
    종료시간을 기준으로 정렬해야 한다.
    가장 빨리 끝나는 회의를 기준으로 시작시간과 비교하여
    최대 회의 개수를 구해야됨

--- BaekjoonAlgorithm/P1931.cpp
+++ BaekjoonAlgorithm/P1931.cpp
@@ -5,55 +5,82 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <map>
+#include <algorithm>
 
 using namespace std;
-long long check_next_conference(int start_time,multimap <int,int> &time_table,multimap <int,int>::iterator &iter);
 
 int main()
 {
-    long long conference_num;
-    long long max_conference = 0;
-    multimap <int, int> time_table;
-    multimap <int,int>::iterator iter;
+    int conference_num;
 
     cin >> conference_num;
 
-    while(conference_num--){
-        int start_time, end_time;
+    vector <pair<int,int>> time_table(conference_num);
 
-        cin >> start_time >> end_time;
-
-        time_table.insert(pair<int,int>(start_time,end_time));
+    for(int i=0;i<conference_num;i++){
+        cin >> time_table[i].second >> time_table[i].first;
     }
 
-    for(iter = time_table.begin();iter != time_table.end();iter++){
-        cout << iter->first << " " << iter->second << endl;
-    }
+    sort(time_table.begin(),time_table.end());
 
-    for(iter = time_table.begin();iter != time_table.end();iter++){
-        long long conference_num;
+    int savetime = 0, max_conference = 0;
 
-        conference_num = 1 + check_next_conference(iter->second,time_table,iter);
-        if(max_conference < conference_num) max_conference = conference_num;
+    for(int i=0;i<conference_num;i++){
+        if(savetime <= time_table[i].second){
+            savetime = time_table[i].first;
+            max_conference++;
+        }
     }
 
     cout << max_conference << endl;
 }
 
-long long check_next_conference(int start_time,multimap <int,int> &time_table,multimap <int,int>::iterator &iter)
-{
-    multimap <int,int>::iterator start_iter = iter;
-    long long max_conference = 0;
+// long long check_next_conference(int start_time,multimap <int,int> &time_table,multimap <int,int>::iterator &iter);
 
-    start_iter++;
-    for(;start_iter != time_table.end();start_iter++){
-        long long conference_num = 0;
-        if(start_iter->first >= start_time){
-            conference_num = 1 + check_next_conference(start_iter->second,time_table,start_iter);
-            if(max_conference < conference_num) max_conference = conference_num;
-        }
-    }
+// int main()
+// {
+//     long long conference_num;
+//     long long max_conference = 0;
+//     multimap <int, int> time_table;
+//     multimap <int,int>::iterator iter;
+
+//     cin >> conference_num;
+
+//     while(conference_num--){
+//         int start_time, end_time;
+
+//         cin >> start_time >> end_time;
+
+//         time_table.insert(pair<int,int>(start_time,end_time));
+//     }
+
+//     for(iter = time_table.begin();iter != time_table.end();iter++){
+//         cout << iter->first << " " << iter->second << endl;
+//     }
+
+//     for(iter = time_table.begin();iter != time_table.end();iter++){
+//         long long conference_num;
+
+//         conference_num = 1 + check_next_conference(iter->second,time_table,iter);
+//         if(max_conference < conference_num) max_conference = conference_num;
+//     }
+
+//     cout << max_conference << endl;
+// }
+
+// long long check_next_conference(int start_time,multimap <int,int> &time_table,multimap <int,int>::iterator &iter)
+// {
+//     multimap <int,int>::iterator start_iter = iter;
+//     long long max_conference = 0;
+
+//     start_iter++;
+//     for(;start_iter != time_table.end();start_iter++){
+//         long long conference_num = 0;
+//         if(start_iter->first >= start_time){
+//             conference_num = 1 + check_next_conference(start_iter->second,time_table,start_iter);
+//             if(max_conference < conference_num) max_conference = conference_num;
+//         }
+//     }
     
-    return max_conference;
-}
+//     return max_conference;
+// }
