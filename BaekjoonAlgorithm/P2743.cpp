2f58edb542a8b6d62895fc42716fa70b6c3c03c1
leehi0110 <maple0110@naver.com>
2020-07-10 15:57:47 +0900

    2020/07/10 Baekjoon Algorithm 문자열 처리 - 단어 길이 재기
    string으로 입력을 받아서 .length() 함수 출력

---
+++ BaekjoonAlgorithm/P2743.cpp
@@ -0,0 +1,21 @@
+// Baekjoon 2743
+// 문제
+// 단어 길이 재기
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s;
+
+    cin >> s;
+
+    cout << s.length() << endl;
+}
