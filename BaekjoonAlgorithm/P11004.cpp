248c02487420dac7dbd87a168cb9812f789377a0
leehi0110 <maple0110@naver.com>
2020-07-14 15:04:03 +0900

    2020/07/14 Baekjoon Algorithm commit check

---
+++ BaekjoonAlgorithm/P11004.cpp
@@ -0,0 +1,22 @@
+// Baekjoon 11004
+// 문제
+// K번째 수
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, k;
+
+    cin >> n >> k;
+    
+    for(int i=0;i<)
+}
