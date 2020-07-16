434cad187c179cd50a9f6dbbc789722a9301ff21
leehi0110 <maple0110@naver.com>
2020-07-16 19:25:35 +0900

    2020/07/16 Baekjoon Algorithm 백트래킹 - N과 M(2)
    N과 M(1)과 같은 방식으로 푼다.
    원소의 순서가 다르더라도 같은것으로 간주하기 때문에
    원소를 deque에 넣을 때 find 함수를 사용하지 않는다

---
+++ BaekjoonAlgorithm/P15650.cpp
@@ -0,0 +1,49 @@
+// Baekjoon 15650
+// 문제
+// 백트래킹
+// N과 M(2)
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <deque>
+
+using namespace std;
+void backtracking_function(deque <int> &container,int full_size,int end_val);
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n,m;
+
+    cin >> n >> m;
+
+    for(int i=1;i<=n;i++){
+        deque <int> container;
+        container.push_back(i);
+
+        backtracking_function(container,m,n);
+
+        container.clear();
+    }
+}
+
+void backtracking_function(deque <int> &container,int full_size,int end_val)
+{
+    if(container.size() == full_size){
+        deque <int>::iterator iter;
+        for(iter = container.begin();iter != container.end();iter++){
+            cout << *iter << " ";
+        }
+        cout << "\n";
+    }
+    else {
+        for(int i=container.back()+1;i<=end_val;i++){
+            container.push_back(i);
+            backtracking_function(container,full_size,end_val);
+            container.pop_back();
+        }
+    }
+}
