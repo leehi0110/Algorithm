7bc5b4ca0e5d573a42bc20382697546d0d137293
leehi0110 <maple0110@naver.com>
2020-07-18 16:58:54 +0900

    2020/07/18 Baekjoon Algorithm 그리디 - 반도체 설계
    반복문을 도는 횟수를 줄여 시간초과 문제를 해결하려고 함
    반례를 통해 예외 케이스를 찾지 못함
    이 문제를 해결하기 위해
    1. LIS알고리즘을 사용한다.
    2. 현재의 알고리즘에서 반례를 찾아내고 시간 복잡도가 향상됐는지 확인

--- BaekjoonAlgorithm/P2352.cpp
+++ BaekjoonAlgorithm/P2352.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <utility>
 #include <algorithm>
 #include <map>
 
@@ -20,28 +21,64 @@ int main()
     int max_cnt = 0;
 
     cin >> n;
-    vector <int> conductor(n);
+
+    vector <pair<int,int>> conductor;
 
     for(int i=0;i<n;i++){
-        cin >> conductor[i];
+        int temp;
+        cin >> temp;
+
+        conductor.push_back(pair<int,int>(temp,i+1));
     }
 
+    sort(conductor.begin(),conductor.end());
+
     for(int i=0;i<n;i++){
-        int left = i+1;
-        int right = conductor[i]+1;
         int cnt = 1;
-
-        while(right <= n){
-            int index = find(conductor.begin(),conductor.end(),right) - conductor.begin();
-            if(index+1 > left){
-                cnt++;
-                left = index+1;
+        int right = conductor[i].second;
+        for(int j=i+1;j<n;j++){
+            if(conductor[j].second > right){
+                cnt ++;
+                right = conductor[j].second;
             }
-            right ++;
         }
-
-        if(cnt > max_cnt) max_cnt = cnt;
+        if(max_cnt < cnt) max_cnt = cnt;
     }
 
     cout << max_cnt << endl;
 }
+
+// int main()
+// {
+//     ios_base :: sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int max_cnt = 0;
+
+//     cin >> n;
+//     vector <int> conductor(n);
+
+//     for(int i=0;i<n;i++){
+//         cin >> conductor[i];
+//     }
+
+//     for(int i=0;i<n;i++){
+//         int left = i+1;
+//         int right = conductor[i]+1;
+//         int cnt = 1;
+
+//         while(right <= n){
+//             int index = find(conductor.begin(),conductor.end(),right) - conductor.begin();
+//             if(index+1 > left){
+//                 cnt++;
+//                 left = index+1;
+//             }
+//             right ++;
+//         }
+
+//         if(cnt > max_cnt) max_cnt = cnt;
+//     }
+
+//     cout << max_cnt << endl;
+// }
