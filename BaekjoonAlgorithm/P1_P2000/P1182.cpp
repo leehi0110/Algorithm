deca466656b8083e129654ce415cd15cbb4fe0ae
leehi0110 <maple0110@naver.com>
2020-07-26 22:25:51 +0900

    2020/07/26 Baekjoon Algorithm 브루트 포스 - 부분수열의 합
    다시 풀 예정

---
+++ BaekjoonAlgorithm/P1_P2000/P1182.cpp
@@ -0,0 +1,28 @@
+// Baekjoon 1182
+// 문제
+// 브루트 포스
+// 부분수열의 합
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, target;
+
+    cin >> n >> target;
+
+    vector <int> number(n);
+
+    for(int i=0;i<n;i++){
+        cin >> number[i];
+    }
+
+    
+}
