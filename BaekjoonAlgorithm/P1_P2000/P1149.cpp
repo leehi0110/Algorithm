23114a10c736c7c25893c604d18f3b1650fd2a61
leehi0110 <maple0110@naver.com>
2020-07-30 23:02:52 +0900

    2020/07/30 Baekjoon Algorithm 다이나믹 프로그래밍 - RGB거리
    2차원 벡터를 이용하여 끝 집부터 차례대로 재귀를 구현해야 할 것 같음
    
    내일 다시 풀 예정

---
+++ BaekjoonAlgorithm/P1_P2000/P1149.cpp
@@ -0,0 +1,26 @@
+// Baekjoon 1149
+// 문제
+// 다이나믹 프로그래밍
+// RGB 거리
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int house_cnt;
+
+    cin >> house_cnt;
+
+    vector <vector<int>> rgb_price;
+
+    for(int i=0;i<house_cnt;i++){
+        vector <int>
+    }
+}
