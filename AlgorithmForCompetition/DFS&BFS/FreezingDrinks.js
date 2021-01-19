70e883cc7ed0d686cd9c8ef4dc3cba4e46632452
SeungNyungLee <maple0110@naver.com>
2021-01-19 12:48:08 +0900

    [DFS & BFS] FreezingDrink
    BFS를 이용해 이중 반복문을 통해 frame[x][y]의 값이 0일 경우 해당 위치의 상하좌우를 재귀함수로 호출한다.
    호출한 결과 방문하지 않은 곳이 있다면 해당 좌표를 기준으로 다시 재귀함수를 호출한다.
    
    이렇게 모든 틀의 원소를 조회해 음료를 얼릴 수 있는 남은 틀의 개수를 구한다.

---
+++ AlgorithmForCompetition/DFS&BFS/FreezingDrinks.js
@@ -0,0 +1,53 @@
+// 제한시간 30 => 10~40
+
+// const frame = [[0,0,1,1,0],[0,0,0,1,1],[1,1,1,1,1],[0,0,0,0,0]];
+// const frame = [
+//   [0,0,0,0,0,1,1,1,1,0,0,0,0,0],
+//   [1,1,1,1,1,1,0,1,1,1,1,1,1,0],
+//   [1,1,0,1,1,1,0,1,1,0,1,1,1,0],
+//   [1,1,0,1,1,1,0,1,1,0,0,0,0,0],
+//   [1,1,0,1,1,1,1,1,1,1,1,1,1,1],
+//   [1,1,0,1,1,1,1,1,1,1,1,1,0,0],
+//   [1,1,0,0,0,0,0,0,0,1,1,1,1,1],
+//   [0,1,1,1,1,1,1,1,1,1,1,1,1,1],
+//   [0,0,0,0,0,0,0,0,0,1,1,1,1,1],
+//   [0,1,1,1,1,1,1,1,1,1,1,0,0,0],
+//   [0,0,0,1,1,1,1,1,1,1,1,0,0,0],
+//   [0,0,0,0,0,0,0,1,1,1,1,0,0,0],
+//   [1,1,1,1,1,1,1,1,1,1,0,0,1,1],
+//   [1,1,1,0,0,0,1,1,1,1,1,1,1,1],
+//   [1,1,1,0,0,0,1,1,1,1,1,1,1,1]
+// ]
+const frame = [
+  [0,0,1],
+  [0,1,0],
+  [1,0,1]
+]
+
+//-----------------------------[input]
+
+const N = frame.length;
+const M = frame[0].length;
+const direction = [[-1,0],[1,0],[0,1],[0,-1]];
+let cnt = 0;
+
+for(let i=0;i<frame.length;i++) {
+  for(let j=0;j<frame[i].length;j++) {
+    let nowX = i;
+    let nowY = j
+
+    const returnVal = recure(N,M,nowX, nowY, frame);
+    if(returnVal !== 0) cnt += 1;
+  }
+}
+
+console.log(cnt);
+
+function recure(N,M,nowX, nowY, frame) {
+  if(nowX >=0 && nowX < N && nowY >=0 && nowY < M) {
+    if(frame[nowX][nowY] === 0) {
+      frame[nowX][nowY] = 1;
+      return 1 + recure(N,M,nowX-1,nowY,frame) + recure(N,M,nowX+1,nowY, frame) + recure(N,M,nowX,nowY-1, frame) + recure(N,M,nowX,nowY+1, frame); 
+    } else return 0;
+  } else return 0;
+}
