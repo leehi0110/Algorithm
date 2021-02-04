0fb2dbfd837df152f83b787fb4566a42933728fd
SeungNyungLee <maple0110@naver.com>
2021-02-04 14:06:18 +0900

    file change

--- ProgrammersAlgorithm/JS/Level_2/SortFileNames.js
+++ ProgrammersAlgorithm/JS/Level_2/SortFileNames.js
@@ -1,76 +1,60 @@
-// function solution(files) {
-//   var answer = [];
-//   var sortArr = [];
+function solution(files) {
+  var answer = [];
+  var sortArr = [];
   
-//   for(let i=0;i<files.length;i++) {
-//     const file = files[i];
-//     answer.push(splitFile(file));
-//   }
+  for(let i=0;i<files.length;i++) {
+    const file = files[i];
+    answer.push(splitFile(file));
+  }
 
-//   answer.sort((a,b) => {
-//     if(a[0].toUpperCase() > b[0].toUpperCase()) return 1;
-//     else if(a[0].toUpperCase < b[0].toUpperCase()) return -1;
-//     else return 0;
-//     // return a[2].toUpperCase() > b[2].toUpperCase();
-//   });
-//   // answer.sort((a,b) => {
-//   //   if(a[0].toUpperCase() === b[0].toUpperCase()) {
-//   //     if(parseInt(a[1]) === parseInt(b[1])) {
-//   //       if(a[2].toUpperCase < b[2].toUpperCase) return true;
-//   //       else return false;
-//   //     } else return parseInt(a[1])- parseInt(b[1]);
-//   //   } else if(a[0].toUpperCase > b[0].toUpperCase) return false;
-//   //   else return true;
-//   // });
+  answer.sort((a,b) => {
+    if(a[0].toUpperCase() > b[0].toUpperCase()) return 1;
+    else if(a[0].toUpperCase() < b[0].toUpperCase()) return -1;
+    else {
+      if(parseInt(a[1]) === parseInt(b[1])) {
+        return 1;
+      } else return parseInt(a[1]) - parseInt(b[1]);
+    }
+  });
 
-//   for(let splitItem of answer) {
-//     sortArr.push(splitItem.join(''));
-//   }
+  for(let splitItem of answer) {
+    sortArr.push(splitItem.join(''));
+  }
 
-//   return sortArr;
-// }
+  return sortArr;
+}
 
-// function splitFile(file) {
-//   let returnArr = [];
-//   let tempStr = '';
-//   let fileChar;
-//   let part = 'head';
+function splitFile(file) {
+  let returnArr = [];
+  let tempStr = '';
+  let fileChar;
+  let part = 'head';
 
-//   for(let i=0;i<file.length;i++) {
-//     fileChar = file[i];
+  for(let i=0;i<file.length;i++) {
+    fileChar = file[i];
 
-//     if(part === 'head') {
-//       if(fileChar >= '0' && fileChar <= '9') {
-//         returnArr.push(tempStr);
-//         tempStr = fileChar;
-//         part = 'number';
-//       } else {
-//         tempStr += fileChar;
-//       }
-//     } else if(part === 'number') {
-//       if(fileChar >= '0' && fileChar <= '9') {
-//         tempStr += fileChar;
-//       } else {
-//         returnArr.push(tempStr);
-//         tempStr = fileChar;
-//         part = 'tail';
-//       }
-//     } else { // part === 'tail'
-//       tempStr += fileChar;
-//     }
+    if(part === 'head') {
+      if(fileChar >= '0' && fileChar <= '9') {
+        returnArr.push(tempStr);
+        tempStr = fileChar;
+        part = 'number';
+      } else {
+        tempStr += fileChar;
+      }
+    } else if(part === 'number') {
+      if(fileChar >= '0' && fileChar <= '9') {
+        tempStr += fileChar;
+      } else {
+        returnArr.push(tempStr);
+        tempStr = fileChar;
+        part = 'tail';
+      }
+    } else { // part === 'tail'
+      tempStr += fileChar;
+    }
 
-//     if(file.length -1 === i) returnArr.push(tempStr);
-//   }
+    if(file.length -1 === i) returnArr.push(tempStr);
+  }
 
-//   console.log(returnArr);
-//   return returnArr;
-// }
-
-let arr = ['C-','B','B-','A-'];
-arr.sort((a,b) => {
-  if(a > b) return 1;
-  else if(a < b) return -1;
-  else return 0;
-});
-
-console.log(arr);
+  return returnArr;
+}
