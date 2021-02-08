e504809ffbc90794435fc52f84413bcdf73bc1ea
SeungNyungLee <maple0110@naver.com>
2021-02-08 11:18:13 +0900

    Day 2: Conditional Statement

--- HackerRank/10DaysOfJavascript/Day_2_ConditionalStatement.js
+++ HackerRank/10DaysOfJavascript/Day_2_ConditionalStatement.js
@@ -10,3 +10,30 @@ function getGrade(score) {
   
   return grade;
 }
+
+function getLetter(s) {
+  let checkLetter = s[0];
+  
+  switch(checkLetter) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+          return 'A';
+      case 'b':
+      case 'c':
+      case 'd':
+      case 'f':
+      case 'g':
+          return 'B';
+      case 'h':
+      case 'j':
+      case 'k':
+      case 'l':
+      case 'm':
+          return 'C';
+      default:
+          return 'D';
+  }
+}
