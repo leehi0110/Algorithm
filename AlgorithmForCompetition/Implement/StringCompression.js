f880cad2aca266e2d7642449d37146bcf5047a30
SeungNyungLee <maple0110@naver.com>
2021-02-09 19:37:52 +0900

    [Implement] String compression
    문자열의 길의 / 2 보다 작게 문자열을 1개부터 분해하고 압축한 길이를 구한 뒤,
    압축된 후 문자열의 길이가 가장 작은 값을 반환하면 된다.

---
+++ AlgorithmForCompetition/Implement/StringCompression.js
@@ -0,0 +1,44 @@
+// 풀이 시간 30 => 18:00 - 18:00
+
+function solution(s) {
+    
+  let min = 99999;
+
+    if(s.length === 1) return 1;
+  
+  for(let i=1;i<=Math.floor(s.length/2);i++) {
+      let temp = [];
+      let tempStr = '';
+      let cnt = 1;
+      
+      for(let j=0;j<s.length;j+=i) {
+          temp.push(s.substr(j,i));
+      }
+      
+      let prevChar = temp[0];
+      for(let j=1;j<temp.length;j++) {
+          let nowChar = temp[j];
+          if(prevChar !== nowChar) {
+              if(cnt === 1) {
+                  tempStr += prevChar;
+              } else {
+                  tempStr += (cnt.toString() + prevChar);
+              }
+              prevChar = nowChar;
+              cnt = 1;
+          } else {
+            cnt ++;
+          }
+
+          if(j === temp.length-1) {
+              if(cnt === 1) {
+                  tempStr += prevChar;
+              } else {
+                  tempStr += (cnt.toString() + prevChar);
+              }
+          }
+      }
+      if(min > tempStr.length) min = tempStr.length;
+  }   
+  return min;
+}
