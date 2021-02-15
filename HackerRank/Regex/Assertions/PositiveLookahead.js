dbc7d4f7262af3aa3736c5de09a3800365a2655c
SeungNyungLee <maple0110@naver.com>
2021-02-15 13:37:36 +0900

    [Regex] Positive Lookahead

---
+++ HackerRank/Regex/Assertions/PositiveLookahead.js
@@ -0,0 +1,17 @@
+var Regex_Pattern = /o(?=oo)/g;
+
+function processData(Test_String) {
+  var Array = Test_String.match(Regex_Pattern);
+  console.log("Number of matches :", Array.length);
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+  _input += input;
+});
+
+process.stdin.on("end", function () {
+  processData(_input);
+});
