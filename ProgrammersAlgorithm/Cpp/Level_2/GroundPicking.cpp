23f47b431e051b9162885323b1aa2b559bbe84b3
SeungNyungLee <maple0110@naver.com>
2021-03-08 11:57:06 +0900

    [DynamicProgramming] 땅따먹기
    재 풀이 필요

---
+++ ProgrammersAlgorithm/Cpp/Level_2/GroundPicking.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getMax(vector<int> land, int index)
+{
+
+  int max = -999;
+
+  for (int i = 0; i < land.size(); i++)
+  {
+    if (i != index)
+    {
+      if (land[i] > max)
+        max = land[i];
+    }
+  }
+
+  return max;
+}
+
+int solution(vector<vector<int>> land)
+{
+  int answer = 0;
+
+  vector<vector<int>> dp(land.size(), vector<int>(4, 0));
+
+  dp[0][0] = land[0][0];
+  dp[0][1] = land[0][1];
+  dp[0][2] = land[0][2];
+  dp[0][3] = land[0][3];
+
+  for (int i = 1; i < land.size(); i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      int maxValue = getMax(land[i - 1], j);
+      dp[i][j] = land[i][j] + maxValue;
+    }
+  }
+
+  for (int i = 0; i < dp.size(); i++)
+  {
+    for (int j = 0; j < dp[i].size(); j++)
+    {
+      cout << dp[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  return answer;
+}
