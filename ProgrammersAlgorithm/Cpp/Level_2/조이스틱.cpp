06863d96ff625a020390496768bff600742de002
SeungNyungLee <maple0110@naver.com>
2021-03-15 18:26:34 +0900

    [Greedy] 조이스틱

--- ProgrammersAlgorithm/Cpp/Level_2/조이스틱.cpp
+++ ProgrammersAlgorithm/Cpp/Level_2/조이스틱.cpp
@@ -6,67 +6,76 @@
 #include <vector>
 
 using namespace std;
-int alphCnt(char target);
-int leftMove(string name, int now);
-int rightMove(string name, int now);
 
-int solution(string name) {
+int solution(string name)
+{
     int answer = 0;
-    int now = 0;
-    string temp = "";
-    string original = name;
-    
-    for(int i=0;i<name.size();i++) {
-        temp += 'A';
-    }
-    
-    while(temp != original) {
-        answer += alphCnt(name[now]); // 조이스틱 위아래 움직임에 대한 횟수
-        temp[now] = name[now]; // AAAA에서 해당 인덱스 문자 변경
-        name[now] = 'A';
-        
-        int left = leftMove(name,now);
-        int right = rightMove(name,now);
-        
-        if(left < right) {
-            answer += left;
-            now = (now - left + name.size())%name.size();
-        }
-        else {
-            answer += right;
-            now = (now+right + name.size()) %name.size();
-        }
-    }
-    
     return answer;
 }
 
-int leftMove(string name, int now) 
-{
-    int cnt = 0;
-    for(int i=0;;i--) {
-        if(i == -1) i = name.size()-1;
-        
-        if(name[i] != 'A') return cnt;
-        else cnt++;
-    }
-}
+// #include <string>
+// #include <vector>
 
-int rightMove(string name, int now)
-{
-    int cnt = 0;
-    for(int i=now;;i++) {
-        if(i == name.size()-1) i = 0;
-        
-        if(name[i] != 'A') return cnt;
-        else cnt ++;
-    }
-}
+// using namespace std;
+// int alphCnt(char target);
+// int leftMove(string name, int now);
+// int rightMove(string name, int now);
 
-int alphCnt(char target)
-{
-    if(target > 'N') return 'Z' - target + 1;
-    else return target - 'A';
-}
+// int solution(string name) {
+//     int answer = 0;
+//     int now = 0;
+//     string temp = "";
+//     string original = name;
 
+//     for(int i=0;i<name.size();i++) {
+//         temp += 'A';
+//     }
 
+//     while(temp != original) {
+//         answer += alphCnt(name[now]); // 조이스틱 위아래 움직임에 대한 횟수
+//         temp[now] = name[now]; // AAAA에서 해당 인덱스 문자 변경
+//         name[now] = 'A';
+
+//         int left = leftMove(name,now);
+//         int right = rightMove(name,now);
+
+//         if(left < right) {
+//             answer += left;
+//             now = (now - left + name.size())%name.size();
+//         }
+//         else {
+//             answer += right;
+//             now = (now+right + name.size()) %name.size();
+//         }
+//     }
+
+//     return answer;
+// }
+
+// int leftMove(string name, int now)
+// {
+//     int cnt = 0;
+//     for(int i=0;;i--) {
+//         if(i == -1) i = name.size()-1;
+
+//         if(name[i] != 'A') return cnt;
+//         else cnt++;
+//     }
+// }
+
+// int rightMove(string name, int now)
+// {
+//     int cnt = 0;
+//     for(int i=now;;i++) {
+//         if(i == name.size()-1) i = 0;
+
+//         if(name[i] != 'A') return cnt;
+//         else cnt ++;
+//     }
+// }
+
+// int alphCnt(char target)
+// {
+//     if(target > 'N') return 'Z' - target + 1;
+//     else return target - 'A';
+// }
