58de78b70c5b6f234b7f3045331fcbfdcbe14e55
SeungNyungLee <maple0110@naver.com>
2021-03-30 17:21:08 +0900

    [Shortest Path] P1504 - 특정한 최단 경로
    플로이드와샬 알고리즘을 이용한 풀이 - 시간 초과

--- BaekjoonAlgorithm/P1_P2000/P1504.cpp
+++ BaekjoonAlgorithm/P1_P2000/P1504.cpp
@@ -22,6 +22,16 @@ int main()
   cin >> N >> E;
 
   vector<vector<pair<int, int>>> graph(N + 1);
+  vector<vector<int>> table(N + 1, vector<int>(N + 1, INF));
+
+  for (int i = 0; i < table.size(); i++)
+  {
+    for (int j = 0l; j < table.size(); j++)
+    {
+      if (i == j)
+        table[i][j] = 0;
+    }
+  }
 
   for (int i = 0; i < E; i++)
   {
@@ -30,5 +40,40 @@ int main()
     cin >> a >> b >> c;
     graph[a].push_back({b, c});
     graph[b].push_back({a, c});
+
+    table[a][b] = c;
+    table[b][a] = c;
+  }
+
+  for (int k = 1; k < N + 1; k++)
+  {
+    for (int i = 1; i < N + 1; i++)
+    {
+      for (int j = 1; j < N + 1; j++)
+      {
+        table[i][j] = min(table[i][j], table[i][k] + table[k][j]);
+        table[j][i] = table[i][j];
+      }
+    }
+  }
+
+  cin >> firstVertex >> secondVertex;
+
+  int first_result = table[1][firstVertex] + table[firstVertex][secondVertex] + table[secondVertex][N];
+  int second_result = table[1][secondVertex] + table[secondVertex][firstVertex] + table[firstVertex][N];
+
+  if (first_result < second_result)
+  {
+    if (first_result == INF)
+      cout << -1 << endl;
+    else
+      cout << first_result << endl;
+  }
+  else
+  {
+    if (second_result == INF)
+      cout << -1 << endl;
+    else
+      cout << second_result << endl;
   }
 }
