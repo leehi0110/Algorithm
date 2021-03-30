7a2b9f5bfd971f2d1e4d7c659fa34554e4ae6380
SeungNyungLee <maple0110@naver.com>
2021-03-30 17:10:23 +0900

    [Shortest Path] P1504 - 특정한 최단 경로
    플로이드와샬을 이용해서 재작성할 예정

--- BaekjoonAlgorithm/P1_P2000/P1504.cpp
+++ BaekjoonAlgorithm/P1_P2000/P1504.cpp
@@ -11,13 +11,9 @@
 using namespace std;
 
 int N, E;
-int first, second;
-int result = 0;
+int firstVertex, secondVertex;
 
-int firstCase = 0;
-int secondCase = 0;
-
-void dijkstra(vector<vector<pair<int, int>>> graph, int start, int end);
+void dijkstra(vector<vector<pair<int, int>>> graph, int start, int end, int &result);
 int main()
 {
   ios_base ::sync_with_stdio(false);
@@ -32,55 +28,7 @@ int main()
     int a, b, c;
 
     cin >> a >> b >> c;
-
     graph[a].push_back({b, c});
     graph[b].push_back({a, c});
   }
-
-  cin >> first >> second;
-
-  dijkstra(graph, 1, first);
-  dijkstra(graph, first, second);
-  dijkstra(graph, second, N);
-
-  if (result == INF)
-    cout << -1 << endl;
-  else
-    cout << result << endl;
 }
-
-void dijkstra(vector<vector<pair<int, int>>> graph, int start, int end)
-{
-  priority_queue<pair<int, int>> pq;
-  vector<int> distance(N + 1, INF);
-
-  pq.push({0, start});
-  distance[start] = 0;
-
-  while (!pq.empty())
-  {
-    int dis = -pq.top().first;
-    int next = pq.top().second;
-    pq.pop();
-
-    if (distance[next] < dis)
-      continue;
-
-    for (int i = 0; i < graph[next].size(); i++)
-    {
-      int cost = distance[next] + graph[next][i].second;
-
-      if (distance[graph[next][i].first] > cost)
-      {
-        distance[graph[next][i].first] = cost;
-        pq.push({-cost, graph[next][i].first});
-      }
-    }
-  }
-
-  result += distance[end];
-}
-
-// 순서에 유념해서 다시 작성
-// 1 -> first vertex -> second Vertex -> N vertex
-// 2 -> second vertex -> first Vertex -> N vertex
