fc0b51e70d00c424bde88a45e157a787a6fe9d24
SeungNyungLee <maple0110@naver.com>
2021-04-04 20:13:05 +0900

    지름길

---
+++ BaekjoonAlgorithm/P1_P2000/P1446.cpp
@@ -0,0 +1,14 @@
+// ProblemNumber || ProblemName : P1446 - 지름길
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+}
