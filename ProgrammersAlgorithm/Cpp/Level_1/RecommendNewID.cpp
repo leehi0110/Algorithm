b2156a0446be9b9c3dd3efbfb566086dcc9a216c
SeungNyungLee <maple0110@naver.com>
2021-04-15 19:10:17 +0900

    [Implement] 신규 아이디 추천

---
+++ ProgrammersAlgorithm/Cpp/Level_1/RecommendNewID.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string new_id)
+{
+  string answer = "";
+
+  return answer;
+}
+
+int main()
+{
+
+  string result = solution("...!@BaT#*..y.abcdefghijklm");
+  // string result = solution("z-+.^.");
+  // string result = solution("=.=");
+}
