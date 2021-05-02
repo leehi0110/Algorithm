c661bb01333d3961e711a2eef1bbf113d9d0d231
SeungNyungLee <maple0110@naver.com>
2021-05-02 14:34:27 +0900

    [Brute Force] P1107 - 리모컨
    이동하고자 하는 채널과 초기 채널과의 거리(1)
    해당 채널과 가장 가까운 작은 채널과의 거리(2)
    해당 채너과 가장 가까운 큰 채널과의 거리(3)
    
    (1)(2)(3) 중 최소값을 찾으면 된다

--- BaekjoonAlgorithm/P1_P2000/P1107.cpp
+++ BaekjoonAlgorithm/P1_P2000/P1107.cpp
@@ -13,58 +13,70 @@ int main()
   ios_base ::sync_with_stdio(false);
   cin.tie(NULL);
 
-  int target, n;
+  int target, n, answer;
+  int plusCnt, leftCnt = INF, rightCnt = INF;
   vector<bool> broken(10, true);
-  vector<int> channel(500001, INF);
 
   cin >> target;
   cin >> n;
 
   for (int i = 0; i < n; i++)
   {
-    int brokenNum;
-    cin >> brokenNum;
+    int num;
+    cin >> num;
 
-    broken[brokenNum] = false;
+    broken[num] = false;
   }
 
-  channel[100] = 0;
+  if (target > 100)
+    plusCnt = target - 100;
+  else
+    plusCnt = 100 - target;
 
-  for (int i = 0; i < 500001; i++)
+  for (int i = target; i < 1000000; i++)
   {
-    if (i < 100)
-      channel[i] = 100 - i;
-    else
-      channel[i] = i - 100;
-  }
+    string s_num = to_string(i);
 
-  for (int i = 0; i < 500001; i++)
-  {
-    string s = to_string(i);
-
-    for (int j = 0; j < s.length(); j++)
+    for (int j = 0; j < s_num.length(); j++)
     {
+      int num = s_num[j] - '0';
 
-      int numS = s[j] - '0';
-
-      if (!broken[numS])
+      if (!broken[num])
         break;
 
-      if (j == s.length() - 1 && channel[i] > s.length())
+      if (j == s_num.length() - 1)
       {
-        channel[i] = s.length();
+        rightCnt = s_num.length() + i - target;
       }
     }
+
+    if (rightCnt != INF)
+      break;
   }
 
-  for (int i = 1; i < 500000; i++)
+  for (int i = target - 1; i >= 0; i--)
   {
-    channel[i] = min(channel[i - 1] + 1, channel[i]);
-    channel[i] = min(channel[i], channel[i + 1] + 1);
+    string s_num = to_string(i);
+
+    for (int j = 0; j < s_num.length(); j++)
+    {
+      int num = s_num[j] - '0';
+
+      if (!broken[num])
+        break;
+
+      if (j == s_num.length() - 1)
+      {
+        leftCnt = s_num.length() + target - i;
+      }
+    }
+
+    if (leftCnt != INF)
+      break;
   }
 
-  for (int i = 0; i < 200; i++)
-  {
-    cout << "Now i : " << i << " " << channel[i] << endl;
-  }
+  answer = min(plusCnt, leftCnt);
+  answer = min(answer, rightCnt);
+
+  cout << answer << endl;
 }
