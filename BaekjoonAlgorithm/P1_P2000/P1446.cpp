c0c2c38b034cb69968b1c2fd20ccc2ec7ec79001
SeungNyungLee <maple0110@naver.com>
2021-05-02 14:50:43 +0900

    [Shortest Path] P1446 - 지름길

--- BaekjoonAlgorithm/P1_P2000/P1446.cpp
+++ BaekjoonAlgorithm/P1_P2000/P1446.cpp
@@ -14,20 +14,27 @@ int main()
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  int n, d;
+  int N, D;
 
-  cin >> n >> d;
-
+<<<<<<< Updated upstream
   vector<int> distance(d + 1, 0);
   vector<vector<pair<int, int>>> graph(d + 1);
 
   for (int i = 0; i <= d; i++)
+=======
+  cin >> N >> D;
+  vector<vector<int>> roads;
+  vector<int> distance(D + 1, 0);
+
+  for (int i = 0; i < D + 1; i++)
+>>>>>>> Stashed changes
   {
     distance[i] = i;
   }
 
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < N; i++)
   {
+<<<<<<< Updated upstream
     int start, end, cost;
 
     cin >> start >> end >> cost;
@@ -63,10 +70,44 @@ int main()
         if (distance[i] + nextCost < distance[next])
         {
           distance[next] = distance[i] + nextCost;
+=======
+    int start, end, dist;
+
+    cin >> start >> end >> dist;
+
+    if (end - start <= dist || end > D)
+      continue;
+    else
+    {
+      roads.push_back({start, end, dist});
+    }
+  }
+
+  for (int i = 0; i < roads.size(); i++)
+  {
+    int start = roads[i][0], end = roads[i][1];
+    int cost = distance[start] + roads[i][2];
+
+    if (distance[end] > cost)
+    {
+      distance[end] = cost;
+      int addDistance = 1;
+      for (int j = end + 1; j < D + 1; j++)
+      {
+        if (distance[j] < distance[end] + addDistance)
+          break;
+        else
+        {
+          distance[j] = distance[end] + addDistance++;
+>>>>>>> Stashed changes
         }
       }
     }
   }
 
+<<<<<<< Updated upstream
   cout << distance[d] << endl;
+=======
+  cout << distance[D] << endl;
+>>>>>>> Stashed changes
 }
