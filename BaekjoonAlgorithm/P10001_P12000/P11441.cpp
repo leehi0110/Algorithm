e69833725eb104fb4d583b27cbec14289e46045a
SeungNyungLee <maple0110@naver.com>
2021-05-10 20:33:31 +0900

    [Cumulative Sum] P11441 - 합 구하기
    누적합을 구한 값들을 vec에 저장한 뒤, [end]의 값에서 [start]까지의 값을 빼면 [start]부터 [end]까지의 값이 나온다

---
+++ BaekjoonAlgorithm/P10001_P12000/P11441.cpp
@@ -0,0 +1,43 @@
+// ProblemNumber || ProblemName : P11441 - 합 구하기
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n, m;
+
+  cin >> n;
+
+  vector<int> vec(n, 0);
+
+  for (int i = 0; i < n; i++)
+  {
+    int num;
+    cin >> num;
+    if (i == 0)
+      vec[i] = num;
+    else
+      vec[i] = vec[i - 1] + num;
+  }
+
+  cin >> m;
+
+  for (int i = 0; i < m; i++)
+  {
+    int start, end;
+    cin >> start >> end;
+
+    if (start > 1)
+      cout << vec[end - 1] - vec[start - 2] << endl;
+    else
+      cout << vec[end - 1] << endl;
+  }
+}
