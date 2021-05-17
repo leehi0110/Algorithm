d4269493fcba89ddfe67db51f0a2d7c25754b0d6
SeungNyungLee <maple0110@naver.com>
2021-05-17 16:05:07 +0900

    [String] P9093 - 단어 뒤집기
    스택을 이용해 문자열을 거꾸로 출력한다

--- BaekjoonAlgorithm/P8001_P10000/P9093.cpp
+++ BaekjoonAlgorithm/P8001_P10000/P9093.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stack>
 
 using namespace std;
 
@@ -11,4 +12,42 @@ int main()
 {
   ios_base ::sync_with_stdio(false);
   cin.tie(NULL);
+
+  int n;
+  stack<char> st;
+
+  cin >> n;
+  cin.ignore();
+  for (int i = 0; i < n; i++)
+  {
+    string temp;
+    getline(cin, temp);
+
+    for (int j = 0; j < temp.length(); j++)
+    {
+      if (temp[j] == ' ')
+      {
+        while (!st.empty())
+        {
+          cout << st.top();
+          st.pop();
+        }
+        cout << " ";
+      }
+      else
+      {
+        st.push(temp[j]);
+      }
+
+      if (j == temp.length() - 1)
+      {
+        while (!st.empty())
+        {
+          cout << st.top();
+          st.pop();
+        }
+        cout << endl;
+      }
+    }
+  }
 }
