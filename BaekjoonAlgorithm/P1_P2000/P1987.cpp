0f47a1f0088796189f175ddbd8c60b5360690dd5
SeungNyungLee <maple0110@naver.com>
2021-05-31 14:24:49 +0900

    [DFS] P1987 - 알파벳
    DFS 알고리즘을 이용한 풀이
    
    풀이방식은 맞지만 DFS()의 파라미터에 vector를 사용할 경우 탐색과정에서 새로운 벡터가 복사되므로 시간복잡도가 증가한다.
    따라서, 변하지 않고 참조만 하는 값일 경우 &vector를 사용한다.
    
    또한, 방문 체크를 하는 alph의 경우 배열 자체가 주소의 속성을 가지고 있기 때문에 네방향에 대한 탐색이 끝나면 다시 사용하지 않은 상태(false)로 되돌린다.

--- BaekjoonAlgorithm/P1_P2000/P1987.cpp
+++ BaekjoonAlgorithm/P1_P2000/P1987.cpp
@@ -1,4 +1,8 @@
 // ProblemNumber || ProblemName : P1987 - 알파벳
+// 하면서 좀 더 깊게 생각하게 된 것들
+// -> DFS에 vector를 call by value로 보낼경우 계속해서 새로운 복사본이 생기기 때문에 시간복잡도가 증가한다.
+// -> 이를 해결하기 위해 배열을 사용하려 했으나 배열 자체가 주소값이기 때문에 다른 방법을 생각해야됨
+// -> [nowY][nowX] 에서 [ny][nx]로 이동할 때 해당 알파벳을 사용한 것으로 체크 한뒤, 네방향에 대한 탐색이 끝나면 다시 사용하지 않은것으로 변경
 
 #include <iostream>
 #include <string>
@@ -10,19 +14,21 @@ using namespace std;
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {-1, 1, 0, 0};
 
+int DFS(bool alph[26], vector<vector<char>> &board, int nowX, int nowY);
+// int DFS(vector<bool> alph, vector<vector<char>> &board, int nowX, int nowY);
+
 int main()
 {
   ios_base ::sync_with_stdio(false);
   cin.tie(NULL);
 
   int n, m;
-  int nowX = 0;
-  int nowY = 0;
-
+  int nowX = 0, nowY = 0; // 말의 위치
   cin >> n >> m;
 
-  vector<bool> alph(26, false);
-  vector<vector<char>> vec(n);
+  vector<vector<char>> board(n); // 알파벳이 적혀있는 보드
+  // vector<bool> alph(26, false);
+  bool alph[26] = {false};
 
   for (int i = 0; i < n; i++)
   {
@@ -32,27 +38,36 @@ int main()
 
     for (int j = 0; j < temp.length(); j++)
     {
-      vec[i].push_back(temp[j]);
+      board[i].push_back(temp[j]);
     }
-  }
+  } // 문자열로 입력을 받아 각 문자를 vector에 입력
 
-  for (auto a : vec)
-  {
-    for (auto b : a)
-    {
-      cout << b << " ";
-    }
-    cout << endl;
-  }
+  cout << DFS(alph, board, nowX, nowY) << "\n";
 }
 
-// 반례
-// 3 5
-// ACDFE
-// BGHKB
-// ABCKZ
+int DFS(bool alph[26], vector<vector<char>> &board, int nowX, int nowY)
+// int DFS(vector<bool> alph, vector<vector<char>> &board, int nowX, int nowY)
+{
+  int max = 1;
 
-// 3 5
-// ACDFE
-// BGHKB
-// KBCKZ
+  alph[board[nowY][nowX] - 65] = true;
+
+  for (int i = 0; i < 4; i++)
+  {
+    int nx = nowX + dx[i];
+    int ny = nowY + dy[i];
+
+    if (nx < 0 || ny < 0 || nx >= board[0].size() || ny >= board.size() || alph[board[ny][nx] - 65])
+      continue;
+    else
+    {
+      int temp = 1 + DFS(alph, board, nx, ny);
+      if (temp >= max)
+        max = temp;
+    }
+  }
+
+  alph[board[nowY][nowX] - 'A'] = false;
+
+  return max;
+}
