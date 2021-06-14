500ebac6c55474ad62b991b01029b18d9a74782f
SeungNyungLee <maple0110@naver.com>
2021-06-14 14:27:49 +0900

    [Priorty Queue + Sort] P1202 - 보석 도둑
    우선선위 큐를 이용해 가치가 높은 보석을 가방에 우선적으로 넣는다.
    정렬된 자료구조인 set과 내장함수인 lower_bound를 이용해 보석을 담을 수 있는 최적의 가방을 찾는다.
    
    가방에 담은 보석의 가치의 MAX값이 int의 범위를 넘어갈 수 있기 때문에 result의 자료형은 long long으로 한다.

--- BaekjoonAlgorithm/P1_P2000/P1202.cpp
+++ BaekjoonAlgorithm/P1_P2000/P1202.cpp
@@ -1,6 +1,7 @@
 // ProblemNumber || ProblemName : P1202 - 보석 도둑
 // 하면서 좀 더 깊게 생각하게 된 것들
-// ->
+// -> 주어진 범위를 주의 깊게 봐야된다.
+// -> 모든 보석의 가치의 합이 int를 넘어갈 수 있기 때문에 long long 을 이용해서 결과를 출력한다.
 
 #include <iostream>
 #include <string>
@@ -8,6 +9,7 @@
 #include <algorithm>
 #include <utility>
 #include <queue>
+#include <set>
 
 using namespace std;
 
@@ -16,14 +18,14 @@ int main()
     ios_base ::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n, k;
-    int result = 0;
+    int gemNum, bagNum;
+    long long result = 0;
     priority_queue<pair<int, int>> gem;
-    vector<int> bag;
+    multiset<int> bag;
 
-    cin >> n >> k;
+    cin >> gemNum >> bagNum;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < gemNum; i++)
     {
         int weight, value;
 
@@ -32,48 +34,29 @@ int main()
         gem.push({value, weight});
     }
 
-    for (int i = 0; i < k; i++)
+    for (int i = 0; i < bagNum; i++)
     {
         int weight;
 
         cin >> weight;
-
-        bag.push_back(weight);
+        bag.insert(weight);
     }
 
-    sort(bag.begin(), bag.end());
-
-    while (!gem.empty())
+    while (!gem.empty() && !bag.empty())
     {
         int value = gem.top().first;
         int weight = gem.top().second;
 
         gem.pop();
 
-        for (int j = 0; j < bag.size(); j++)
+        auto it = bag.lower_bound(weight);
+
+        if (it != bag.end())
         {
-            if (bag[j] >= weight)
-            {
-                result += value;
-                bag.erase(bag.begin() + j);
-                break;
-            }
+            result += static_cast<long long>(value);
+            bag.erase(it);
         }
     }
 
     cout << result << endl;
 }
-
-// 3 2
-// 1 65
-// 5 23
-// 2 99
-// 10
-// 2
-
-// 3 2
-// 5 97
-// 4 98
-// 5 99
-// 5
-// 4
