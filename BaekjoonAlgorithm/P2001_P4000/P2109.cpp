742ffedef68baa38666671fa0c10d220af796a50
SeungNyungLee <maple0110@naver.com>
2021-06-20 20:03:14 +0900

    [Priority Queue] P2109 - 순회강연
    우선순위큐에 데이터를 삽입할 때 어떤 기준을 세울지 잘 정해야된다.
    또한, find(rbegin(),rend(),element) 를 이용해 특정 원소가 있는지 뒤에서부터 체크할 수 있다

--- BaekjoonAlgorithm/P2001_P4000/P2109.cpp
+++ BaekjoonAlgorithm/P2001_P4000/P2109.cpp
@@ -1,6 +1,6 @@
 // ProblemNumber || ProblemName : P2109 - 순회강연
 // 하면서 좀 더 깊게 생각하게 된 것들
-// ->
+// -> find(vector.rbegin(), vector.rend(), element)를 이용해 뒤에서 처음으로 나오는 위치를 찾을 수 있다.
 
 #include <iostream>
 #include <string>
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <queue>
 #include <utility>
+#include <set>
 
 using namespace std;
 
@@ -19,10 +20,11 @@ int main()
   int n;
   int result = 0;
   priority_queue<pair<int, int>> pq;
-  vector<bool> arr(10001, true);
+  vector<bool> vaildDay(100001, true);
 
   cin >> n;
-  arr[0] = false;
+
+  vaildDay[0] = false;
 
   for (int i = 0; i < n; i++)
   {
@@ -40,42 +42,16 @@ int main()
 
     pq.pop();
 
-    auto it = find(arr.rbegin() + (arr.size() - day), arr.rend(), true);
-    if (it == arr.rend())
+    auto it = find(vaildDay.rbegin() + (vaildDay.size() - day - 1), vaildDay.rend(), true);
+
+    if (it == vaildDay.rend())
       continue;
     else
     {
-      arr[it - arr.begin()] = true;
+      *it = false;
       result += pay;
     }
-
-    // 반복문 없이 요일이 비었는지 확인하는 방법?
-
-    // for (int i = day; i > 0; i--)
-    // {
-    //   if (find(arr.begin(), arr.end(), i) == arr.end())
-    //   {
-    //     result += pay;
-    //     arr.push_back(i);
-    //     break;
-    //   }
-    // }
   }
 
   cout << result << endl;
 }
-
-// 3
-// 1 1
-// 2 10
-// 2 10
-// 답 = 20
-
-// 7
-// 20 1
-// 2 1
-// 10 3
-// 100 2
-// 8 2
-// 5 20
-// 50 10
