ffbf8476925353811dff02e7e62bfe64d80afe1f
SeungNyungLee <maple0110@naver.com>
2021-06-24 18:50:03 +0900

    [BFS] P10026 - 적록색약
    너비우선탐색법을 이용해 영역의 개수를 구한다

--- BaekjoonAlgorithm/P10001_P12000/P10026.cpp
+++ BaekjoonAlgorithm/P10001_P12000/P10026.cpp
@@ -9,6 +9,10 @@
 
 using namespace std;
 
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+void bfs(vector<vector<string>> &map, int y, int x, int area, string color);
+
 int main()
 {
   ios_base ::sync_with_stdio(false);
@@ -18,25 +22,67 @@ int main()
 
   cin >> n;
 
-  vector<vector<char>> map(n);
+  vector<vector<string>> map(n);
+  vector<vector<string>> simpleMap(n);
 
   for (int i = 0; i < n; i++)
   {
     string temp;
 
     cin >> temp;
+
     for (int j = 0; j < temp.length(); j++)
     {
-      map[i].push_back(temp[j]);
+      string tmp = temp.substr(j, 1);
+
+      map[i].push_back(tmp);
+
+      if (tmp == "G")
+        simpleMap[i].push_back("R");
+      else
+        simpleMap[i].push_back(tmp);
     }
   }
 
+  int area = 0;
+  int simpleArea = 0;
+
   for (int i = 0; i < map.size(); i++)
   {
-    for (int j = 0; j < map.size(); j++)
+    for (int j = 0; j < map[i].size(); j++)
     {
-      cout << map[i][j] << " ";
+      if (map[i][j] == "R" || map[i][j] == "G" || map[i][j] == "B")
+      {
+        area++;
+        bfs(map, i, j, area, map[i][j]);
+      }
+
+      if (simpleMap[i][j] == "R" || simpleMap[i][j] == "B")
+      {
+        simpleArea++;
+        bfs(simpleMap, i, j, area, simpleMap[i][j]);
+      }
     }
-    cout << endl;
+  }
+
+  cout << area << " " << simpleArea << endl;
+}
+
+void bfs(vector<vector<string>> &map, int y, int x, int area, string color)
+{
+
+  map[y][x] = to_string(area);
+
+  for (int i = 0; i < 4; i++)
+  {
+    int moveX = x + dx[i];
+    int moveY = y + dy[i];
+
+    if (moveX < 0 || moveY < 0 || moveX >= map[0].size() || moveY >= map.size() || map[moveY][moveX] != color)
+      continue;
+    else
+    {
+      bfs(map, moveY, moveX, area, color);
+    }
   }
 }
