e8d5e3023c50ef3a0979a49123443585990df917
SeungNyungLee <maple0110@naver.com>
2021-06-30 19:24:57 +0900

    [Segment Tree] P2042 - 구간 합 구하기
    100%에서 틀린답으로 나온다. 어디가 틀렸는지 알 수 없어 질문 게시판 확인 후 수정 예정

--- BaekjoonAlgorithm/P2001_P4000/P2042.cpp
+++ BaekjoonAlgorithm/P2001_P4000/P2042.cpp
@@ -8,33 +8,35 @@
 #include <algorithm>
 
 using namespace std;
-int init(vector<long long> &tree, vector<int> arr, int start, int end, int node);
-long long sum(vector<long long> tree, int start, int end, int left, int right, int node);
-void update(vector<long long> &tree, int start, int end, int node, int index, int diff);
+typedef long long ll;
+
+ll init(int start, int end, int node);
+ll sum(int start, int end, int node, int left, int right);
+void update(int start, int end, int node, int index, ll diff);
+
+int n, m, k;
+vector<ll> arr;
+ll tree[4 * 1000002];
 
 int main()
 {
   ios_base ::sync_with_stdio(false);
   cin.tie(NULL);
 
-  int n, m, k;
-
   cin >> n >> m >> k;
-
-  vector<int> arr;
-  vector<long long> tree(n * 4);
   // n = 숫자의 개수
   // m = 수의 변경이 일어나는 횟수
   // k = 구간의 합을 구하는 횟수
 
   for (int i = 0; i < n; i++)
   {
-    int temp;
-    cin >> temp;
-    arr.push_back(temp);
+    ll number;
+    cin >> number;
+
+    arr.push_back(number);
   }
 
-  init(tree, arr, 0, arr.size() - 1, 1);
+  init(0, arr.size() - 1, 1);
 
   for (int i = 0; i < m + k; i++)
   {
@@ -43,50 +45,50 @@ int main()
     cin >> a >> b >> c;
 
     if (a == 1)
-    { // b번째 수를 c로 바꾼다
-      update(tree, 1, arr.size(), 1, b, c - arr[b - 1]);
-      arr[b - 1] = c;
+    {
+      // 값 변경
+      update(0, arr.size() - 1, 1, b - 1, static_cast<ll>(c) - arr[b - 1]);
+      arr[b - 1] = static_cast<ll>(c);
     }
     else
-    { // b번째 수부터 c번째 수까지의 구간합을 구한다.
-      cout << sum(tree, 0, arr.size() - 1, b - 1, c - 1, 1) << "\n";
+    {
+      // 구간 합 출력
+      cout << sum(0, arr.size() - 1, 1, b - 1, c - 1) << "\n";
     }
   }
 }
 
-void update(vector<long long> &tree, int start, int end, int node, int index, int diff)
+ll init(int start, int end, int node)
 {
-  if (index < start || index > end)
-    return;
-  tree[node] += static_cast<long long>(diff);
-
   if (start == end)
-    return;
-
+    return tree[node] = arr[start];
   int mid = (start + end) / 2;
 
-  update(tree, start, mid, node * 2, index, diff);
-  update(tree, mid + 1, end, node * 2 + 1, index, diff);
+  return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
 }
 
-long long sum(vector<long long> tree, int start, int end, int left, int right, int node)
+ll sum(int start, int end, int node, int left, int right)
 {
   if (left > end || right < start)
     return 0;
-  else if (left <= start && right >= end)
+
+  if (left <= start && end <= right)
     return tree[node];
-  else
-  {
-    int mid = (start + end) / 2;
-    return sum(tree, start, mid, left, right, node * 2) + sum(tree, mid + 1, end, left, right, node * 2 + 1);
-  }
+
+  int mid = (start + end) / 2;
+  return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
 }
 
-int init(vector<long long> &tree, vector<int> arr, int start, int end, int node)
+void update(int start, int end, int node, int index, ll diff)
 {
+  if (index < start || index > end)
+    return;
+
+  tree[node] += diff;
   if (start == end)
-    return tree[node] = static_cast<long long>(arr[start]);
+    return;
   int mid = (start + end) / 2;
 
-  return tree[node] = static_cast<long long>(init(tree, arr, start, mid, node * 2) + init(tree, arr, mid + 1, end, node * 2 + 1));
+  update(start, mid, node * 2, index, diff);
+  update(mid + 1, end, node * 2 + 1, index, diff);
 }
