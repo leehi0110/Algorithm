94dcf93d451053e4cf5c9514ca3c6d6aabe545fe
SeungNyungLee <maple0110@naver.com>
2021-08-02 14:41:12 +0900

    [Graph - BFS] P4179 - 불!
    지훈이의 움직임과 불의 움직임을 각각 BFS 알고리즘으로 구한 뒤,
    지훈이가 불보다 먼저 가장자리에 도착하는 경우 중 가장 작은 시간을 구한다.
    
    이때, 지훈이는 1명이기 때문에 하나의 입력으로만 들어오지만 불은 여러개일 수 있기때문에 여러 입력으로 들어온다

--- BaekjoonAlgorithm/P4001_P6000/P4179.cpp
+++ BaekjoonAlgorithm/P4001_P6000/P4179.cpp
@@ -1,6 +1,7 @@
 // ProblemNumber || ProblemName : P4179 - 불!
 // 하면서 좀 더 깊게 생각하게 된 것들
-// ->
+// -> 조건을 잘 읽고 문제를 명확하게 이해하자
+// -> J(지훈)이는 하나만 들어오지만 F(불)은 여러개 들어올 수 있다.
 
 #include <iostream>
 #include <string>
@@ -8,6 +9,8 @@
 #include <algorithm>
 #include <queue>
 #include <utility>
+#include <queue>
+#define INF 1e9
 
 using namespace std;
 
@@ -16,12 +19,19 @@ int n, m;
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
-char map[1001][1001];
+int jihunMap[1001][1001];
+int fireMap[1001][1001];
+
+int jihunStart[2];
+vector<pair<int, int>> fireStart;
+
+int result = INF;
 
 int main()
 {
   ios_base ::sync_with_stdio(false);
   cin.tie(NULL);
+  priority_queue<pair<int, pair<int, int>>> pq;
 
   cin >> n >> m;
 
@@ -33,7 +43,104 @@ int main()
 
     for (int j = 0; j < temp.length(); j++)
     {
-      map[i][j] = temp[j];
+      if (temp[j] == '#')
+      {
+        jihunMap[i][j] = -1;
+        fireMap[i][j] = -1;
+      }
+      else if (temp[j] == 'J')
+      {
+        jihunMap[i][j] = 1;
+        jihunStart[0] = i;
+        jihunStart[1] = j;
+        fireMap[i][j] = INF;
+      }
+      else if (temp[j] == 'F')
+      {
+        fireMap[i][j] = 1;
+        fireStart.push_back({i, j});
+        // fireStart[0] = i;
+        // fireStart[1] = j;
+        jihunMap[i][j] = INF;
+      }
+      else
+      {
+        jihunMap[i][j] = INF;
+        fireMap[i][j] = INF;
+      }
     }
   }
+
+  pq.push({-1, {jihunStart[0], jihunStart[1]}});
+
+  while (!pq.empty())
+  {
+    int dist = -pq.top().first;
+    int nowX = pq.top().second.first;
+    int nowY = pq.top().second.second;
+
+    pq.pop();
+
+    for (int i = 0; i < 4; i++)
+    {
+      int nextX = nowX + dx[i];
+      int nextY = nowY + dy[i];
+
+      if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m || jihunMap[nextX][nextY] == -1 || jihunMap[nextX][nextY] <= dist + 1)
+        continue;
+      else
+      {
+        jihunMap[nextX][nextY] = dist + 1;
+        pq.push({-jihunMap[nextX][nextY], {nextX, nextY}});
+      }
+    }
+  }
+
+  for (int i = 0; i < fireStart.size(); i++)
+  {
+    pq.push({-1, {fireStart[i].first, fireStart[i].second}});
+  }
+
+  while (!pq.empty())
+  {
+    int dist = -pq.top().first;
+    int nowX = pq.top().second.first;
+    int nowY = pq.top().second.second;
+
+    pq.pop();
+
+    for (int i = 0; i < 4; i++)
+    {
+      int nextX = nowX + dx[i];
+      int nextY = nowY + dy[i];
+
+      if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m || fireMap[nextX][nextY] == -1 || fireMap[nextX][nextY] <= dist + 1)
+        continue;
+      else
+      {
+        fireMap[nextX][nextY] = dist + 1;
+        pq.push({-fireMap[nextX][nextY], {nextX, nextY}});
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      if (i == 0 || j == 0 || i == n - 1 || j == m - 1)
+      {
+        // cout << i << j << endl;
+        if (jihunMap[i][j] < fireMap[i][j] && result > jihunMap[i][j])
+        {
+          result = jihunMap[i][j];
+        }
+      }
+    }
+  }
+
+  if (result == INF)
+    cout << "IMPOSSIBLE" << endl;
+  else
+    cout << result << endl;
 }
