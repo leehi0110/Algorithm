3d78fc41e2c95eabb0881e20c94882eb8ad39fd8
SeungNyungLee <maple0110@naver.com>
2021-08-23 21:16:08 +0900

    [Implement] P11942 - 고려대는 사랑입니다

---
+++ BaekjoonAlgorithm/P10001_P12000/P11942.cpp
@@ -0,0 +1,18 @@
+// ProblemNumber || ProblemName : P11942 - 고려대는 사랑입니다
+// 하면서 좀 더 깊게 생각하게 된 것들
+// ->
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cout << "고려대학교" << endl;
+}
