21a7561e19eefd048b31c5619482c9999bbd687b
SeungNyungLee <maple0110@naver.com>
2021-09-18 15:38:42 +0900

    [Implement] P15802 - 타노스

---
+++ BaekjoonAlgorithm/P14001_/P15802.cpp
@@ -0,0 +1,20 @@
+// ProblemNumber || ProblemName : P15802 - 타노스
+// 하면서 좀 더 깊게 생각하게 된 것들
+// ->
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cout << 0 << endl;
+}
