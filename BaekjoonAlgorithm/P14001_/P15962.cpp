ba8f614fb71e2b70385c534a239fe5a0b4f6e6e0
Leehi0110 <allabout@leeho0110.local>
2021-10-01 14:09:52 +0900

    [Implement] P15962 - 새로운 시작

---
+++ BaekjoonAlgorithm/P14001_/P15962.cpp
@@ -0,0 +1,21 @@
+// ProblemNumber || ProblemName : P15962 - 새로운 시작
+// 하면서 좀 더 깊게 생각하게 된 것들
+// ->
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cout << "파이팅!!" << endl;
+}
