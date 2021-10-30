1676e4034c9f4fc29ce379c2c2ca162ddff71981
Leehi0110 <maple0110@naver.com>
2021-10-30 17:36:34 +0900

    [Implement] P14645 - 와이버스 부릉부릉

---
+++ BaekjoonAlgorithm/P14001_/P14645.cpp
@@ -0,0 +1,30 @@
+// ProblemNumber || ProblemName : P14645 - 와이버스 부릉부릉
+// 하면서 좀 더 깊게 생각하게 된 것들
+// ->
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n, k;
+
+  cin >> n >> k;
+
+  for(int i=0;i<n;i++) {
+    int a, b;
+    cin >> a >> b;
+  }
+
+  cout << "비와이" << endl;
+}
